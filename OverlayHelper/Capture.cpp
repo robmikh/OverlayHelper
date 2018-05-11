26a1170901726dc1af419583d1355ee3b6e9c569
Robert Mikhayelyan <romikh@microsoft.com>
2018-05-11 10:33:37 -0700

    Fix tabs >:(

--- OverlayHelper/Capture.cpp
+++ OverlayHelper/Capture.cpp
@@ -33,21 +33,21 @@ SimpleCapture::SimpleCapture(
     // Don't return from the constructor until our work has finished on
     // the capture thread.
     auto initialized = std::make_shared<safe_flag>();
-	// If we were to just capture the item and device in the lambda, we
-	// would end up smuggling the objects to the other thread and get a 
-	// marshalling error. Use an agile ref to get around this.
-	auto itemAgile = agile_ref(m_item);
-	auto deviceAgile = agile_ref(m_device);
+    // If we were to just capture the item and device in the lambda, we
+    // would end up smuggling the objects to the other thread and get a 
+    // marshalling error. Use an agile ref to get around this.
+    auto itemAgile = agile_ref(m_item);
+    auto deviceAgile = agile_ref(m_device);
     auto success = m_dispatcherQueue.TryEnqueue([=, &initialized]() -> void
     {
-		auto itemTemp = itemAgile.get();
-		auto deviceTemp = deviceAgile.get();
+        auto itemTemp = itemAgile.get();
+        auto deviceTemp = deviceAgile.get();
 
         m_framePool = Direct3D11CaptureFramePool::Create(
-			deviceTemp,
+            deviceTemp,
             DirectXPixelFormat::B8G8R8A8UIntNormalized,
             2,
-			itemTemp.Size());
+            itemTemp.Size());
         m_session = m_framePool.CreateCaptureSession(itemTemp);
         m_lastSize = itemTemp.Size();
         m_framePool.FrameArrived({ this, &SimpleCapture::OnFrameArrived });
